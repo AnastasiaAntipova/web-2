e65ef353f75e6f4f477cd2d64e590361317028b7
Anastasia Antipova <antnastya03@gmail.com>
2024-03-13 00:25:25 +0300

    DB initialization

--- server/server.js
+++ server/server.js
@@ -1,6 +1,25 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require('express');
+const cors =require("cors");
+const app = express();
+const port = 3001;
+
+app.use(cors());
+
+app.use(express.json());
+
+const dbConfig = require("./config/db.config");
+
+const db = require("./models");
+
+db.mongoose.connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(()=>{
+    console.log("DB is connected")
+}).catch(()=>{
+    console.log("DB isn't connected");
+    process.exit();
+})
 
 app.get('/', (req, res) => {
     res.send('Good morning World!')
@@ -9,3 +28,21 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+/*const express = require('express');
+const cors =require("cors");
+const app = express();
+const port = 3001;
+
+app.use(cors());
+
+app.use(express.json());
+
+
+
+app.get('/', (req, res) => {
+    res.send('Good morning World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})*/
