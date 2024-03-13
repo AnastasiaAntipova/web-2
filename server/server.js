07261bb85999d620b0f8f51a0bc4144eaaee9221
Anastasia Antipova <antnastya03@gmail.com>
2024-03-13 19:39:14 +0300

    DB admin adding

--- server/server.js
+++ server/server.js
@@ -1,26 +1,49 @@
-const express = require('express');
-const cors =require("cors");
-const app = express();
-const port = 3001;
+const express = require('express');//
+const cors =require("cors");//
+const app = express();//
+const port = 3001;//
 
-app.use(cors());
+app.use(cors());//
 
-app.use(express.json());
+app.use(express.json());//
 
-const dbConfig = require("./config/db.config");
+const dbConfig = require("./config/db.config");//
 
-const db = require("./models");
+const db = require("./models");//
+const User = db.user//
 
 db.mongoose.connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(()=>{
-    console.log("DB is connected")
-}).catch(()=>{
-    console.log("DB isn't connected");
+    console.log("DB is connected");
+    initial();
+}).catch((err)=>{
+    console.log("Error. DB isn't connected", err);
     process.exit();
 })
 
+
+function initial(){
+    User.estimatedDocumentCount((err, usersCount)=>{
+        console.log("user's count is", usersCount);
+        if (!err && usersCount === 0){
+            new User ({ username: "Admin",
+                nickname: "Admin",
+                email: "Admin",
+                password: "root",
+            }).save(err=>{
+                    if(err){
+                        console.log("Admin isn't created", err)
+                    } else {
+                        console.log("Admin is created");
+                    }
+            })
+
+        }
+    })
+}
+
 app.get('/', (req, res) => {
     res.send('Good morning World!')
 })
@@ -28,21 +51,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
-/*const express = require('express');
-const cors =require("cors");
-const app = express();
-const port = 3001;
-
-app.use(cors());
-
-app.use(express.json());
-
-
-
-app.get('/', (req, res) => {
-    res.send('Good morning World!')
-})
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})*/
