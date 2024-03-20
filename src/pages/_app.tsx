3151089007e7718a5e38574618bb99534f829d66
Anastasia Antipova <antnastya03@gmail.com>
2024-03-21 02:16:23 +0300

    add mui theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import {createTheme, CssBaseline, Palette, ThemeProvider} from "@mui/material";
+import {red} from "@mui/material/colors";
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
 }
+
+const theme = createTheme();
